e5b4d0b4cee6aa3f425981af1d093bda55f62120
Eddi777 <Gjhnfkbot1>
2022-04-22 16:27:40 +0300

    1.1.4 work commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,130 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private Session session;
+    private final Util util;
+
+    public UserDaoHibernateImpl() {
+        util = new Util();
     }
 
 
     @Override
     public void createUsersTable() {
-
+        String query = "CREATE TABLE IF NOT EXISTS user " +
+                "(id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(50) NOT NULL, " +
+                "lastname VARCHAR(50) NOT NULL, " +
+                "age SMALLINT NOT NULL" +
+                "PRIMARY KEY (id))";
+        session = util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(query).addEntity(User.class);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("User table was dropped");
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String query = "DROP TABLE IF EXIST user;";
+        session = util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(query).addEntity(User.class);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("User table was dropped");
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        session = util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            System.out.println("Save user exception " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        session = util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+            System.out.println("User под номером – " + id + " удален из базы данных {" + user + "}");
+        } catch (Exception e) {
+            System.out.println("User by ID is unavailable");
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        session = util.getSessionFactory().openSession();
+        List<User> usersDBList = null;
+        try {
+            session.beginTransaction();
+            CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
+            criteriaQuery.from(User.class);
+            usersDBList = session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            System.out.println("Remove user by ID exception " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            session.close();
+            if (usersDBList.size() == 0) {
+                return null;
+            } else {
+                return usersDBList;
+            }
+        }
     }
-
     @Override
     public void cleanUsersTable() {
-
+        session = util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
+            criteriaQuery.from(User.class);
+            final List<User> usersDBList = session.createQuery(criteriaQuery).getResultList();
+            for (Object obj : usersDBList) {
+                session.delete(obj);
+            }
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Remove user by ID exception " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            session.close();
+            util.closeSessionFactory();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,38 +1,47 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 
 public class Util {
-    private final String dbConf = "jdbc:mysql://localhost:3306/test";
-    private final String user = "root";
-    private final String password = "Root@12345";
-    private Connection connection = null;
-    public Connection getConnection() {
-        if (connection != null) {
-            return connection;
+
+    public SessionFactory sessionFactory = null;
+
+    public SessionFactory getSessionFactory() {
+        if (sessionFactory != null) {
+            return sessionFactory;
         }
+        final Configuration config = new Configuration()
+                .setProperty("connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("connection.url", "jdbc:mysql://localhost:3306/test")
+                .setProperty("connection.username", "root")
+                .setProperty("connection.password", "Root@12345")
+                .setProperty("dialect", "org.hibernate.dialect.MySQLDialect")
+                .setProperty("current_session_context_class", "thread")
+                .setProperty("connection.pool_size", "1")
+                .setProperty("hbm2ddl.auto" ,"create-drop")
+                .setProperty("show_sql", "true")
+                .addClass(User.class);
+
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure(config)
+                .build();
         try {
-            connection = DriverManager.getConnection(dbConf, user, password);
-            return connection;
-        } catch (SQLException e) {
-            System.out.println("DB connection exception");
-            e.printStackTrace();
+
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            return sessionFactory;
+        } catch (Exception e) {
+            System.out.println("DataBase session opening exception " + e.getMessage());
+            StandardServiceRegistryBuilder.destroy(registry);
             return null;
         }
     }
-    public void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-                connection = null;
-            } catch (SQLException e) {
-                System.out.println("DB closing exception");
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("DB is closed");
-        }
+    public void closeSessionFactory(){
+        sessionFactory.close();
     }
 }
